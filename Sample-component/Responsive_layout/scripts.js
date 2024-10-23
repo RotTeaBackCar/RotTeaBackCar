9e98727268c6f1ba3f726c761c8263bc8d63360b
Yoga B <ybudhi21@gmail.com>
2024-10-23 11:01:31 +0700

    finalizing componen

---
+++ Sample-component/Responsive_layout/scripts.js
@@ -0,0 +1,5 @@
+const resizeBtn = document.getElementById('resize');
+resizeBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.body.classList.toggle('sb-expand');
+});
